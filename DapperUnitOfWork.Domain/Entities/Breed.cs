bce4fbe4698c88911bfd22e64037b4ced2f90d1b
timschreiber <timschreiber@users.noreply.github.com>
2015-02-21 16:37:37 -0500

    Created Unit of Work and Repositories

---
+++ DapperUnitOfWork.Domain/Entities/Breed.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperUnitOfWork.Domain.Entities
+{
+    public class Breed
+    {
+        public Guid BreedId { get; set; }
+        public string Name { get; set; }
+    }
+}
