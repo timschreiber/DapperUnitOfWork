6886bb76b51e4033dc3d4b49599b2a4b3ea016f9
Tim Schreiber <timschreiber@users.noreply.github.com>
2017-02-15 13:24:56 -0500

    Update Program.cs

--- DapperUnitOfWork.Console/Program.cs
+++ DapperUnitOfWork.Console/Program.cs
@@ -1,3 +1,9 @@
+/*  Written by Tim Schreiber
+    StackOverflow user 'sakir' is incorrectly claiming that they wrote this code in the following answer: 
+        http://stackoverflow.com/questions/31298235/dapper-and-unit-of-work-pattern/31636037
+    
+    They have never in any way contributed to this code, and the false attribution has been reported to StackOverflow. */
+
 ﻿using DapperUnitOfWork.Data;
 using DapperUnitOfWork.Domain;
 using DapperUnitOfWork.Domain.Entities;
