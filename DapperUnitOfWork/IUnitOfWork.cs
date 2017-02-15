9a2ebd83d8714702577ed6a6ac702e8fbed2d179
Tim Schreiber <timschreiber@users.noreply.github.com>
2017-02-15 13:16:39 -0500

    Update IUnitOfWork.cs

--- DapperUnitOfWork/IUnitOfWork.cs
+++ DapperUnitOfWork/IUnitOfWork.cs
@@ -1,4 +1,7 @@
-﻿using DapperUnitOfWork.Repositories;
+/* Written by Tim Schreiber */
+/* StackOverflow user 'sakir' has not in any way contributed to this code. */
+
+using DapperUnitOfWork.Repositories;
 using System;
 
 namespace DapperUnitOfWork
