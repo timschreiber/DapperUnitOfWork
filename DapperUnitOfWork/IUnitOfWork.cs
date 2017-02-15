a7ba493a3c9f2b9135ff54003ea91ec8e7787442
Tim Schreiber <timschreiber@users.noreply.github.com>
2017-02-15 13:22:47 -0500

    Update IUnitOfWork.cs

--- DapperUnitOfWork/IUnitOfWork.cs
+++ DapperUnitOfWork/IUnitOfWork.cs
@@ -1,5 +1,8 @@
-/* Written by Tim Schreiber */
-/* StackOverflow user 'sakir' has not in any way contributed to this code. */
+/*  Written by Tim Schreiber
+    StackOverflow user 'sakir' is incorrectly claiming that they wrote this code in the following answer: 
+        http://stackoverflow.com/questions/31298235/dapper-and-unit-of-work-pattern/31636037
+    
+    They have never in any way contributed to this code, and the false attribution has been reported to StackOverflow. */
 
 using DapperUnitOfWork.Repositories;
 using System;
