4e5d401ed03583ff9ba6a1262175f8b200bb5380
Tim Schreiber <timschreiber@users.noreply.github.com>
2017-02-15 13:24:16 -0500

    Update Cat.cs

--- DapperUnitOfWork/Entities/Cat.cs
+++ DapperUnitOfWork/Entities/Cat.cs
@@ -1,3 +1,9 @@
+/*  Written by Tim Schreiber
+    StackOverflow user 'sakir' is incorrectly claiming that they wrote this code in the following answer: 
+        http://stackoverflow.com/questions/31298235/dapper-and-unit-of-work-pattern/31636037
+    
+    They have never in any way contributed to this code, and the false attribution has been reported to StackOverflow. */
+
 ﻿namespace DapperUnitOfWork.Entities
 {
     public class Cat
