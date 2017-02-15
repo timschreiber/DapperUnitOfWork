a63b7789585c08f3d771ad852900c1cc06403c92
Tim Schreiber <timschreiber@users.noreply.github.com>
2017-02-15 13:23:56 -0500

    Update Breed.cs

--- DapperUnitOfWork/Entities/Breed.cs
+++ DapperUnitOfWork/Entities/Breed.cs
@@ -1,4 +1,10 @@
-﻿namespace DapperUnitOfWork.Entities
+/*  Written by Tim Schreiber
+    StackOverflow user 'sakir' is incorrectly claiming that they wrote this code in the following answer: 
+        http://stackoverflow.com/questions/31298235/dapper-and-unit-of-work-pattern/31636037
+    
+    They have never in any way contributed to this code, and the false attribution has been reported to StackOverflow. */
+
+namespace DapperUnitOfWork.Entities
 {
     public class Breed
     {
