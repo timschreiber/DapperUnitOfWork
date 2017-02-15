a401e83a7b769363597aac140b6e8e92ab6635cb
Tim Schreiber <timschreiber@users.noreply.github.com>
2017-02-15 13:16:20 -0500

    Update UnitOfWork.cs

--- DapperUnitOfWork/UnitOfWork.cs
+++ DapperUnitOfWork/UnitOfWork.cs
@@ -1,4 +1,7 @@
-﻿using DapperUnitOfWork.Repositories;
+/* Written by Tim Schreiber */
+/* StackOverflow user 'sakir' has not in any way contributed to this code. */
+
+using DapperUnitOfWork.Repositories;
 using System;
 using System.Data;
 using System.Data.SqlClient;
